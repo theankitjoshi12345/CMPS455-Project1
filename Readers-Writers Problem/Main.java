04024cc52bb1c9d693e7da18d4f083d93e4fcb59
Ankit Joshi <theankitjoshi12345@gmail.com>
2026-02-11 00:52:48 -0600

    Read-Write Problem Coding Done

--- Readers-Writers Problem/Main.java
+++ Readers-Writers Problem/Main.java
@@ -2,30 +2,31 @@ import java.util.Scanner;
 import java.util.concurrent.Semaphore;
 
 public class Main {
-    public static Semaphore area = new Semaphore(1);
-    public static Semaphore mutex = new Semaphore(1); // For doing readersCount operations
-    public static Semaphore readersGate = new Semaphore(1); // For stoping readers after maxReaders
-    public static int readers;
-    public static int writers;
-    public static int maxReaders;
-    public static int readersCount = 0; // It will be used to make sure no more than 10 threads will read a file at one time
+    public static Semaphore sharedFile = new Semaphore(1);
+    public static Semaphore wTurn = new Semaphore(0); // Priority will be given to readers instead.
+    public static Semaphore rTurn = new Semaphore(1);
+    public static Semaphore mutex = new Semaphore(1); // Used to manipulate number of readers agents accessing.
+
+    public static int readingAgents;
+    public static int coordinatingAgents;
+    public static int maxReadingAgents; 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter number of readers: ");
-        readers = in.nextInt();
-        System.out.print("Enter number of writers: ");
-        writers = in.nextInt();
-        System.out.print("Enter maximum number of readers: ");
-        maxReaders = in.nextInt();
+        System.out.print("Enter number of Reading Agents (Non-negative Integers only): ");
+        readingAgents = in.nextInt(); // Can be any non-zero integers only
+        System.out.print("Enter number of Coordinating Agents (Non-negative Integers only): ");
+        coordinatingAgents = in.nextInt(); // Can be any non-zero integers only
+        System.out.print("Enter maximum number of Reading Agents (Non-zero Integers only): ");
+        maxReadingAgents = in.nextInt(); // Can be any positive integers equal to or less than reading agents.
         in.close();
 
-        for (int i = 0; i < readers; i++) {
-            Read r = new Read(i);
+        for (int i = 0; i < readingAgents; i++) {
+            ReadingThread r = new ReadingThread(i);
             r.start();
         }
-        for (int j = 0; j < writers; j++) {
-            Write w = new Write(j);
-            w.start();
+        for (int j = 0; j < coordinatingAgents; j++) {
+            CoordinatingThread c = new CoordinatingThread(j);
+            c.start();
         }
     }
 }
