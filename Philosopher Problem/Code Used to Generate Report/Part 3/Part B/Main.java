d1e717df3a14182bdcb69cfff69a3a35a92f0684
Ankit Joshi <theankitjoshi12345@gmail.com>
2026-02-10 23:02:18 -0600

    Task1 Code Completed, Observation Remaining

---
+++ Philosopher Problem/Code Used to Generate Report/Part 3/Part B/Main.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+import java.util.concurrent.Semaphore;
+
+public class Main {
+
+    // Declaring global variables which will be shared across threads.
+    public static int totalPhilosophers;
+    // public static int totalMeals;
+
+    // public static Semaphore[] chopsticks;
+    public static Semaphore semHold = new Semaphore(0);
+    public static Semaphore mutex = new Semaphore(1);
+
+    public static void main(String args[]) {
+        long start_time = System.nanoTime();
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the number of philosophers: ");
+        totalPhilosophers = in.nextInt();
+        // System.out.print("Enter total number of meals: ");
+        // totalMeals = in.nextInt();
+        // totalMeals = totalPhilosophers * 4;
+        in.close();
+
+        // # of chopsticks = # of philosophers
+        // chopsticks = new Semaphore[totalPhilosophers];
+        Philosopher[] philosophers = new Philosopher[totalPhilosophers];
+        Thread[] threads = new Thread[totalPhilosophers];
+    
+        // All chopsticks are initialized to semaphore value 1
+        // for (int i = 0; i < chopsticks.length; i++) {
+        //    chopsticks[i] = new Semaphore(1);
+        // }
+
+        for (int i = 0; i < totalPhilosophers; i++) {
+            philosophers[i] = new Philosopher(i);
+            threads[i] = new Thread(philosophers[i]);
+            threads[i].start();
+        }
+
+        // IMPORTANT: wait for all philosophers to finish
+        for (int i = 0; i < totalPhilosophers; i++) {
+            try {
+                threads[i].join();
+            } catch (Exception e) {
+                System.out.println("Exception has occured: "+ e);
+            }
+        }
+                
+        long end_time = System.nanoTime();
+        System.out.printf("Runtime in milliseconds = "); 
+        System.out.println((end_time - start_time) / 1000000.0);
+
+        System.out.println("Last line has been executed in \"main.java\".");
+    }
+}
---
+++ Philosopher Problem/Code Used to Generate Report/Part 3/Part B/Philosopher.java
@@ -0,0 +1,81 @@
+import java.util.Random;
+
+public class Philosopher implements Runnable {
+    public static int waitingPhilosophers = 0;
+    private static Random rand = new Random();
+
+    public int philosopherNumber;
+    public int meals = 0;
+
+    public Philosopher(int philosopherNumber) {
+        this.philosopherNumber = philosopherNumber;
+    }
+
+    @Override
+    public void run() {
+        waitForOtherPhilosophers();
+        System.out.println("P" + philosopherNumber + " sit in the table.");
+
+        while (meals < 4) {
+
+            try {
+                // Main.chopsticks[philosopherNumber].acquire();
+                System.out.println("P" + philosopherNumber + " pick up left chopstick.");
+                // Main.chopsticks[(philosopherNumber + 1) % Main.totalPhilosophers].acquire();
+                System.out.println("P" + philosopherNumber + " pick up right chopstick.");
+
+            } catch (Exception e) {
+                System.out.println("Exception has occured: "+ e);
+            }
+
+            // eat
+            System.out.println("P" + philosopherNumber + " start eating M" + meals + ".");
+            waiting();
+            meals++;
+
+            // Main.chopsticks[philosopherNumber].release();
+            System.out.println("P" + philosopherNumber + " put down left chopstick.");
+            // Main.chopsticks[(philosopherNumber + 1) % Main.totalPhilosophers].release();
+            System.out.println("P" + philosopherNumber + " put down right chopstick.");
+
+            // think
+            System.out.println("P" + philosopherNumber + " start thinking.");
+            waiting();
+        }
+        
+        waitForOtherPhilosophers();
+        System.out.println("P" + philosopherNumber + " left the table.");
+    }
+
+    public void waiting() {
+        // waiting for 3 to 6 cycles at random
+        for (int k = 0; k < rand.nextInt(4) + 3; k++) {
+            // Nothing.
+        }
+    }
+    
+    public void waitForOtherPhilosophers() {
+        try {
+            Main.mutex.acquire();
+        } catch (Exception e) {
+            System.out.println("Exception has occured: "+ e);
+        }
+        waitingPhilosophers++;
+        if (waitingPhilosophers == Main.totalPhilosophers) {
+            for (int i = 0; i < Main.totalPhilosophers - 1; i++) {
+                Main.semHold.release();
+            }
+            waitingPhilosophers = 0;
+            Main.mutex.release();
+        } else {
+            Main.mutex.release();
+            try {
+                Main.semHold.acquire();
+            } catch (Exception e) {
+                System.out.println("Exception has occured: "+ e);
+            }
+        }
+        
+    }
+
+}
